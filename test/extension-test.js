774cb7a6da4b72b5378bb071dd1fba42bec70039
Christian Johansen <christian@gitorious.org>
2012-02-23 00:07:47 +0100

    Buster extension for HTML doc

---
+++ test/extension-test.js
@@ -0,0 +1,33 @@
+var buster = require("buster");
+var ext = require("../lib/extension");
+var config = require("buster-configuration");
+
+function process(group, then, errBack) {
+    group.resolve().then(function (resourceSet) {
+        resourceSet.serialize().then(then, errBack);
+    }, errBack);
+}
+
+buster.testCase("HTML doc extension", {
+    setUp: function () {
+        this.config = config.create();
+    },
+
+    "processes tests to extract html doc": function (done) {
+        var group = this.config.addGroup("Some tests", {
+            resources: [{
+                path: "/buster.js",
+                content: "function () { /*:DOC el = <p></p>*/ }"
+            }],
+            tests: ["/buster.js"]
+        });
+
+        ext.create().configure(group);
+
+        process(group, done(function (serialized) {
+            assert.match(serialized.resources[0].content,
+                         "document.createElement");
+        }.bind(this)), buster.log);
+    }
+});
+
---
+++ test/parser-test.js
@@ -0,0 +1,19 @@
+var buster = require("buster");
+var parser = require("../lib/parser");
+
+buster.testCase("HTML doc parser", {
+    "leaves non-html doc code untouched": function () {
+        var code = parser.parse("function () { return 42; }");
+        assert.equals(code, "function () { return 42; }");
+    },
+
+    "leaves unparsable code untouched": function () {
+        var code = parser.parse("function () { return 42");
+        assert.equals(code, "function () { return 42");
+    },
+
+    "extracts html docs and updates code": function () {
+        var code = parser.parse("function () { /*:DOC element = <div></div>*/ }");
+        assert.match(code, "document.createElement");
+    }
+});
